603bbed44cb432d95e14079a465a8312e260e568
Oghale Etomu <oghale.etomu@leatherback.co>
2023-01-03 11:12:17 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{Component.title}</title>
+        <meta name="description" content={Component.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
