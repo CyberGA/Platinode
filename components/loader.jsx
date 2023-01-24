bb61838def94ceaefe646ac8169a1943844b2cb3
CyberGA <etomu.joshua@gmail.com>
2023-01-24 21:31:10 +0000

    donate to a project

--- components/loader.jsx
+++ components/loader.jsx
@@ -5,7 +5,7 @@ const AppLoader = () => {
     <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
       <Loader color="cyan.3" />
       <p className="mt-[20px] font-epilogue font-bold text-[20px] text-white text-center">
-        Transaction is in progress <br /> Please wait...
+        Please wait...
       </p>
     </div>
   );
